3d6bafcee0cd3ecf552efea70285e1926d469f52
K R V Prasad <rvprasad.kothuri@gmail.com>
2018-11-21 21:42:09 +0530

    Adding project

---
+++ Desktop/Axeligent/userclass/userclass/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Userclass
+{
+    class Program
+    {
+        //GIVEN CODE
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                var user = new User();
+                Console.WriteLine("please enter the username, or q to exit");
+                var userName = Console.ReadLine();
+                if (userName == "q")
+                {
+                   break;
+                }
+                user.Add(userName);
+                Console.WriteLine($"number of addedUser {user.GetUsersCount()}");
+            }
+
+        }
+        //MY CODE 
+        class User
+        {
+            //Declaring a static List to store multiple entries
+            private static List<string> users = new List<string>();
+            //Add Function to append, entered username to list
+            public void Add(string userName)
+            {
+                //Adding the username at as the last item in the list
+                users.Add(userName);
+            }
+            public int GetUsersCount()
+            {
+                //Returns number of usernames stored in List
+                return users.Count;
+            }
+        }
+
+    }       
+}
